0162bcea3113683fb0bd46f6f49981a0d6a1f834
Alex Nohrin <beterok@inbox.ru>
2023-07-15 09:05:15 +0500

    Added tests API

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -3,6 +3,7 @@ package ru.netology;
 import org.junit.Assert;
 import org.junit.Test;
 import ru.netology.service.CashbackHackService;
+import org.junit.jupiter.api.Assertions;
 
 
 public class CashbackHackServiceTest {
@@ -55,4 +56,51 @@ public class CashbackHackServiceTest {
         Assert.assertEquals(expected, actual);
     }
 
+    @org.junit.jupiter.api.Test
+    public void shouldHowMuchBuyIfPurchaseLess1000API() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = 900;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldHowMuchBuyIfPurchase1000API() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = 1_000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldHowMuchBuyIfPurchaseMore1000API() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = 1_300;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldHowMuchBuyIfPurchase0API() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int amount = 0;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1_000;
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
